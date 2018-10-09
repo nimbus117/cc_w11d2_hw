b85330f6aea37091889f576c157c78aca2b988b6
nimbus117 <mail@nimbus117.co.uk>
2018-10-09 17:02:04 +0100

    added canTakeBook test

--- src/test/java/BorrowerTest.java
+++ src/test/java/BorrowerTest.java
@@ -5,13 +5,29 @@ import static org.junit.Assert.assertEquals;
 
 public class BorrowerTest {
     private Borrower borrower;
+    private Library library;
+    private Book book1;
+    private Book book2;
     @Before
     public void before() {
         borrower = new Borrower();
+        library = new Library(2);
+        book1 = new Book("LOTR", "J. R. R. Tolkien", "Fantasy");
+        book2 = new Book("Dragonriders of Pern", "Anne McCaffrey", "Fantasy");
+        library.addBook(book1);
+        library.addBook(book2);
     }
 
     @Test
     public void hasEmptyCollection() {
         assertEquals(0, borrower.countBooks());
     }
+
+    @Test
+    public void canTakeBook() {
+        borrower.takeBook(library, book2);
+        assertEquals(1, library.countBooks());
+        assertEquals(1, borrower.countBooks());
+    }
 }
+
