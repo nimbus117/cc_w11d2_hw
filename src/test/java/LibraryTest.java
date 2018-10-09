ee2def714588b71d278463eb9780da7557ba68e6
nimbus117 <mail@nimbus117.co.uk>
2018-10-09 16:44:45 +0100

    added cantAddBookWhenCapacityFull test

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -6,10 +6,14 @@ import static org.junit.Assert.assertEquals;
 public class LibraryTest {
     private Library library;
     private Book book1;
+    private Book book2;
+    private Book book3;
     @Before
     public void before() {
-        library = new Library();
+        library = new Library(2);
         book1 = new Book("LOTR", "J. R. R. Tolkien", "Fantasy");
+        book2 = new Book("Dragonriders of Pern", "Anne McCaffrey", "Fantasy");
+        book3 = new Book("The Dreaming Void", "Peter F. Hamilton", "Sci-Fi");
     }
 
     @Test
@@ -21,5 +25,13 @@ public class LibraryTest {
     public void canAddBook() {
         library.addBook(book1);
         assertEquals(1, library.countBooks());
+    }
+    @Test
+
+    public void cantAddBookWhenCapacityFull() {
+        library.addBook(book1);
+        library.addBook(book2);
+        library.addBook(book3);
+        assertEquals(2, library.countBooks());
     }
 }
