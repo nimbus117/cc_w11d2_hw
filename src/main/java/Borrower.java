4d2def967a1b1823324e1d4ecff6801049b1eb1c
nimbus117 <mail@nimbus117.co.uk>
2018-10-09 17:00:47 +0100

    passed borrower hasEmptyCollection test

--- src/main/java/Borrower.java
+++ src/main/java/Borrower.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+
 public class Borrower {
+
+    private ArrayList<Book> borrowedBooks;
+
+    public Borrower() {
+        this.borrowedBooks = new ArrayList<>();
+    }
+
+    public int countBooks() {
+        return borrowedBooks.size();
+    }
 }
