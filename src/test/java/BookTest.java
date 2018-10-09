f7010c2221f765671c72157160e9032fdef8e141
nimbus117 <mail@nimbus117.co.uk>
2018-10-09 16:28:33 +0100

    passed basic book tests

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -4,10 +4,10 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class BookTest {
-    Book book1;
+    private Book book1;
     @Before
     public void before() {
-        Book book1 = new Book("LOTR", "J. R. R. Tolkien", "Fantasy")
+        book1 = new Book("LOTR", "J. R. R. Tolkien", "Fantasy");
     }
 
     @Test
