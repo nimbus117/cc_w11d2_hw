68bed10a0660885d1eecd45b330754702f811a17
nimbus117 <mail@nimbus117.co.uk>
2018-10-09 16:34:46 +0100

    passed hasEmptyColletion test

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+
 public class Library {
+    private ArrayList<Book> bookCollection;
+
+    public Library() {
+        bookCollection = new ArrayList<>();
+    }
+
+    public int countBooks() {
+        return bookCollection.size();
+    }
 }
