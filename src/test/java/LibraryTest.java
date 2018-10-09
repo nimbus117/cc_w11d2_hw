a1423d7250ce2d513c15491fb767edc90cd49334
nimbus117 <mail@nimbus117.co.uk>
2018-10-09 16:38:09 +0100

    added canAddBook test

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -4,14 +4,22 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class LibraryTest {
-    Library library;
+    private Library library;
+    private Book book1;
     @Before
     public void before() {
         library = new Library();
+        book1 = new Book("LOTR", "J. R. R. Tolkien", "Fantasy");
     }
 
     @Test
     public void hasEmptyColletion() {
         assertEquals(0, library.countBooks());
+    }
+
+    @Test
+    public void canAddBook() {
+        library.addBook(book1);
+        assertEquals(1, library.countBooks());
     }
 }
